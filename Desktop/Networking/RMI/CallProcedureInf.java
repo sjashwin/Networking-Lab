4d95c538a03247bcaf683d3b8295ea4ce98ec9bd
Balasubramanian <balathinnappan@gmail.com>
2015-03-25 00:27:40 +0530

    Update CallProcedureInf.java

--- Desktop/Networking/RMI/CallProcedureInf.java
+++ Desktop/Networking/RMI/CallProcedureInf.java
@@ -7,6 +7,10 @@
 * @since 19-03-2014 
 */
 import java.rmi.*;
+/*
+*  This interface provides the 
+*  the routines for remote object.
+*/
 public interface CallProcedureInf extends Remote {
 	public int add( int x, int y )throws RemoteException ;
 	public int sub( int x, int y )throws RemoteException ;
